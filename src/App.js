68181531bc0cac80e21ca634226913f6b5d0e94c
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-10-23 18:41:20 -0300

    Adding task form and toggle the appearence of this

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import {useState} from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() { 
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id:1,
@@ -27,12 +29,25 @@ function App() {
   const deleteTask = (id) =>{
     setTasks(tasks.filter((task) => task.id !== id))
   }
+
+  const toggleReminder = (id) =>{
+    setTasks(tasks.map((task)=> task.id === id 
+    ? {...task, reminder: !task.reminder}
+    : task
+    ))
+  }
+  const addTask = (task) =>{
+    const id = Math.floor(Math.random()*10000)+1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
   
   return (
-    <div className='container'>
-      <Header/>
+    <div className ='container'>
+      <Header onAdd = {()=> setShowAddTask(!showAddTask)}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 
-      ? (<Tasks tasks={tasks} onDelete={deleteTask}/>)
+      ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>)
       : ('No Tasks To Show')}
     </div>
   );
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,14 @@
 import { Task } from "./Task"
-const Tasks = ({tasks, onDelete}) => {
+const Tasks = ({tasks, onDelete, onToggle}) => {
   return (
     <>
       {tasks.map((task)=>(
-        <Task key={task.id} task={task.text} onDelete={onDelete}/>
+        <Task key={task.id}  
+        task={task.text} 
+        onDelete={onDelete} 
+        onToggle={onToggle}/>
         ))}
     </>
-
   )
 }
 export default Tasks
