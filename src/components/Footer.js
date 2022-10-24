6e191a3dcf5510ab5a4158bd4ba6e93983c7a0f3
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-10-23 21:45:15 -0300

    Updating own css and footer copyright icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer>
-      <p>Copyright &copy: 2022</p>
+      <p>Copyright &copy; : 2022</p>
       <a href="https://github.com/Ianduha13/taskTracker" target={"_blank"} rel='noreferrer'>About</a>
     </footer>
   )
